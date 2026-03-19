01e872f750da3db11928a9cec523613a19cb1c77
Santyrc0325 <scamachor@uniempresarial.edu.co>
2026-03-18 23:13:50 -0500

    Spring Initializr

--- logistics-api/src/main/java/com/sinergia/logistics/LogisticsApiApplication.java
+++ logistics-api/src/main/java/com/sinergia/logistics/LogisticsApiApplication.java
@@ -1,13 +1,13 @@
-package backend;
+package com.sinergia.logistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class LogisticsApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(LogisticsApiApplication.class, args);
 	}
 
 }
--- logistics-api/src/test/java/com/sinergia/logistics/LogisticsApiApplicationTests.java
+++ logistics-api/src/test/java/com/sinergia/logistics/LogisticsApiApplicationTests.java
@@ -1,10 +1,10 @@
-package backend;
+package com.sinergia.logistics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class LogisticsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
